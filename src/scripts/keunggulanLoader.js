f48bd44580bfa2943e30582468cdea07960ebfdb
mystudy <ekowirsabits@gmail.com>
2026-06-26 21:40:10 +0700

    Update keunggulanLoader.js

--- src/scripts/keunggulanLoader.js
+++ src/scripts/keunggulanLoader.js
@@ -1,11 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// Konfigurasi Supabase
-const SUPABASE_URL = "https://jqeithheusmnrxtoaisk.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZWl0aGhldXNtbnJ4dG9haXNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzODczNDQsImV4cCI6MjA3NTk2MzM0NH0._STcPPBqU6-_CfSAakcQ7DlwcSa3iOswRYaF2Ec57-U";
+import { supabase } from "./supabase.js";
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function loadKeunggulan() {
   try {
